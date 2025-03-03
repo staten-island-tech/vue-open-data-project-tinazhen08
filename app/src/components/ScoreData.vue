4b690a1df7eb37a6d700b848049b56cbe90931b9
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-03-03 13:57:20 -0500

    Create ScoreData.vue

---
+++ app/src/components/ScoreData.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+const data = ref('')
+async function getData(){
+    let res = await fetch(`https://data.cityofnewyork.us/resource/f9bf-2cp4.json`)
+    let d = await res.json()
+    data.value = d
+}
+
+onMounted(()=> {
+    getData();
+})
+</script>
+
+<style scoped>
+
+</style>
