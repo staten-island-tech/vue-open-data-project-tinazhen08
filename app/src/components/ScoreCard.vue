e7eb423447196f2eb67c61cd799bf184ae3c05e5
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-03-10 13:40:43 -0400

    check data is fetched

---
+++ app/src/components/ScoreCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>{{ scores.school_name }}</h2>
+        <h3>{{ id }}</h3>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+const props = defineProps({
+    scores: Object,
+    id: Number
+})
+</script>
+
+<style scoped>
+
+</style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <ScoreCard
+      v-for="(score, index) in scores"
+      :key="index"
+      :scores="score"
+      :id="index+1"
+    />
   </main>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import ScoreCard from "@/components/ScoreCard.vue";
+import { Bar } from 'vue-chartjs'
+const scores = ref('')
+async function getData(){
+    let res = await fetch(`https://data.cityofnewyork.us/resource/f9bf-2cp4.json`)
+    let data = await res.json()
+    scores.value = data
+}
+
+onMounted(() => {
+    getData();
+})
+</script>
